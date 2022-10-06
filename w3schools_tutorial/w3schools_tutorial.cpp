e33e2d5a9a01b4bdeff79b4732a5f0b8bff8fd95
gyungmin08 <gooodgoguma@naver.com>
2022-10-06 19:56:27 +0900

    if else

--- w3schools_tutorial/w3schools_tutorial.cpp
+++ w3schools_tutorial/w3schools_tutorial.cpp
@@ -231,6 +231,43 @@ int Bool() {
 	return 0;
 }
 
+int IfElse() {
+	if (20 > 18) {
+		cout << "20 is greater than 18";
+	}
+
+	int x = 20, y = 18;
+	if (x > y) {
+		cout << "x is greater than y";
+	}
+
+	int time = 20;
+	if (time < 18) { 
+		cout << "Good day.";
+	} 
+	else {
+		cout << "Good evening.";
+	}
+
+	if (time < 18) { cout << "Good day."; }
+	else { cout << "Good evening."; }
+
+	if (time < 10) {
+		cout << "Good morning.";
+	}
+	else if (time < 20) {
+		cout << "Good day.";
+	}
+	else {
+		cout << "Good evening.";
+	}
+
+	string result = (time < 18) ? "Good day." : "Good evening.";
+	cout << result
+
+	return 0;
+}
+
 int main() {
 	/*
 	cout << Syntax();
@@ -248,6 +285,8 @@ int main() {
 	cout << Math();
 	cout << Bool();
 	*/
+
+	cout << IfElse();
 
 	cout << "\n\n";
 }
